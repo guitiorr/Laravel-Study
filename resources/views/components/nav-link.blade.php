c1fd338739488f7a11a2deec0f3677545f2573da
guitiorr <officialglude12345@gmail.com>
2024-09-12 20:01:32 +0700

    put active into props

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,6 @@
+@props(['active' => false])
+
+
 <a  {{ $attributes }}
     class=" {{ $active ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
     aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
