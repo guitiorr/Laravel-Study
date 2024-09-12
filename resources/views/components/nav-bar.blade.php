4cfe8d4d45b8fe75fa3320e715a3df3f973ad9bf
guitiorr <officialglude12345@gmail.com>
2024-09-12 17:12:12 +0700

    converted request()->is('/') to be reusable ''

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -12,7 +12,7 @@
                         {{-- <a href="/"
                             class=" {{ request() -> is('/') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
                             aria-current="page">Home</a> --}}
-                            <x-nav-link href="/">Home</x-nav-link>
+                            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
                         <a href="/blog"
                         class=" {{ request() -> is('blog') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}">Blog</a>
                         <a href="/about"
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,3 @@
 <a  {{ $attributes }}
-    class=" {{ request()->is('/') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
-    aria-current="{{ request()->is('/') ? 'page' : false }}">{{ $slot }}</a>
+    class=" {{ $active ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
+    aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
