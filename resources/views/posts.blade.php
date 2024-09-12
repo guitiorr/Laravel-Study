1d9878d34d3c109370e9e4dc0d6f975b422de73e
guitiorr <officialglude12345@gmail.com>
2024-09-12 20:48:50 +0700

    added id for posts, nested post title inside an anchor tag to be clickable with the same destination as read more

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -30,14 +30,16 @@
     @foreach($posts as $post)
 
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post ['title'] }}</h2>
+        <a href="/posts/{{ $post ['id'] }}" class="hover:underline">
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post ['title'] }}</h2>
+        </a>
         <div class="text-base text-gray-500">
             <a href="#">{{ $post ['author'] }}</a> | {{ $post ['date'] }}
         </div>
         <p class="my-4 font-light">
             {{ Str::limit($post ['body'], 230) }}
         </p>
-        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+        <a href="/posts/{{ $post ['id'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
     </article>
 
     {{-- <article class="py-8 max-w-screen-md border-b border-gray-300">
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,14 @@ Route::get('/about', function() {
 Route::get('/posts', function() {
     return view('posts', ['title' => 'Posts', 'posts' => [
         [
+            'id' => '1',
             'title' => 'Judul Artikel 1',
             'author' => 'Cristiano Budi',
             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quidem perspiciatis facilis porro, dolorem nemo neque, fugiat sequi dolores nulla delectus, ipsa asperiores culpa! Eveniet labore non perferendis rerum esse?',
             'date' => '1 January 2024'
         ],
         [
+            'id' => 2,
             'title' => 'Judul Artikel 2',
             'author' => 'Cristiano Budi',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sapiente eligendi harum. Expedita quia, voluptates totam odit distinctio voluptas beatae, nam maiores optio est at, eum impedit dolor velit unde?',
