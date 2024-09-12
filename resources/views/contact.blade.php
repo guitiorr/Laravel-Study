2f3b1e4b5fb43a3b1f0d0877063c3e4c4f26e51c
guitiorr <officialglude12345@gmail.com>
2024-09-12 13:30:08 +0700

    add contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,10 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact</title>
+    <link rel="stylesheet" href="css/stylecontact.css">
 </head>
 <body>
     <a href="/">Home</a>
     <a href="/about">About</a>
     <a href="/blog">Blog</a>
+    <a href="/contact" class="">Contact</a>
+
+    <div class="head">
+        <h1 class="">Reach out to me!</h1>
+    </div>
+
+    <div class="github">
+        <img src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" alt="" class="">
+        <a href="https://github.com/guitiorr" class="">GitHub/guitiorr</a>
+    </div>
+
 </body>
 </html>
