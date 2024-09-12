b763c742884086640bb75224be08ce24d92a850c
guitiorr <officialglude12345@gmail.com>
2024-09-12 12:59:58 +0700

    basic template

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>About page</h1>
+    <h3 class="">Owner: @php
+        echo $nama
+    @endphp </h3>
+    <img src="img/jem.png" alt="Gambar jem" class="">
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Home Page</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
