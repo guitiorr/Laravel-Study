d933f95b40d941cfab9fa2e99ff2e9d757575aed
guitiorr <officialglude12345@gmail.com>
2024-09-13 12:21:13 +0700

    move Post class into models folder and set namespace

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+class Post{
+    public static function all(){
+        return [
+            [
+                'id' => '1',
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Cristiano Budi',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quidem perspiciatis facilis porro, dolorem nemo neque, fugiat sequi dolores nulla delectus, ipsa asperiores culpa! Eveniet labore non perferendis rerum esse?',
+                'date' => '1 January 2024'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Cristiano Budi',
+                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sapiente eligendi harum. Expedita quia, voluptates totam odit distinctio voluptas beatae, nam maiores optio est at, eum impedit dolor velit unde?',
+               'date' => '1 February 2024'
+            ]
+            ];
+    }
+}
