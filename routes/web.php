50e09ab51150edd0f810239765165a0a88de6fb7
guitiorr <officialglude12345@gmail.com>
2024-09-13 12:11:07 +0700

    Put post data inside class

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,29 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Post{
+    public static function all(){
+        return [
+            [
+                'id' => '1',
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Cristiano Budi',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quidem perspiciatis facilis porro, dolorem nemo neque, fugiat sequi dolores nulla delectus, ipsa asperiores culpa! Eveniet labore non perferendis rerum esse?',
+                'date' => '1 January 2024'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Cristiano Budi',
+                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sapiente eligendi harum. Expedita quia, voluptates totam odit distinctio voluptas beatae, nam maiores optio est at, eum impedit dolor velit unde?',
+               'date' => '1 February 2024'
+            ]
+            ];
+    }
+}
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
@@ -16,24 +39,7 @@ Route::get('/about', function() {
 });
 
 Route::get('/posts', function() {
-    return view('posts', ['title' => 'Posts', 'posts' => [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Cristiano Budi',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quidem perspiciatis facilis porro, dolorem nemo neque, fugiat sequi dolores nulla delectus, ipsa asperiores culpa! Eveniet labore non perferendis rerum esse?',
-            'date' => '1 January 2024'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Cristiano Budi',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sapiente eligendi harum. Expedita quia, voluptates totam odit distinctio voluptas beatae, nam maiores optio est at, eum impedit dolor velit unde?',
-           'date' => '1 February 2024'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Posts', 'posts' => Post::all()]);
 });
 
 Route::get('/posts/{slug}', function($slug){
