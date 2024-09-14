064a8daf61b0bd62afb538c4d03f6316920a037f
guitiorr <officialglude12345@gmail.com>
2024-09-14 16:58:59 +0700

    added route model binding

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -30,7 +30,7 @@
     @foreach($posts as $post)
 
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <a href="/posts/{{ $post ['id'] }}" class="hover:underline">
+        <a href="/posts/{{ $post ['slug'] }}" class="hover:underline">
             <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post ['title'] }}</h2>
         </a>
         <div class="text-base text-gray-500">
@@ -39,7 +39,7 @@
         <p class="my-4 font-light">
             {{ Str::limit($post ['body'], 150) }}
         </p>
-        <a href="/posts/{{ $post ['id'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+        <a href="/posts/{{ $post ['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
     </article>
 
     {{-- <article class="py-8 max-w-screen-md border-b border-gray-300">
