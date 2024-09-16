e712d3047c79a264419cbaf2b53ad4ce0e02bca1
guitiorr <officialglude12345@gmail.com>
2024-09-16 13:53:34 +0700

    forgot to add foreign key in user model, now updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
     }
 
     public function posts(): HasMany{
-        return $this->hasMany(Post::class);
+        return $this->hasMany(Post::class, 'author_id');
     }
 
 }
