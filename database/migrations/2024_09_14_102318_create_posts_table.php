36e53435778e747b86304bd0f96586aa27140a8e
guitiorr <officialglude12345@gmail.com>
2024-09-16 13:34:09 +0700

    add user id as foreign key in posts

--- database/migrations/2024_09_14_102318_create_posts_table.php
+++ database/migrations/2024_09_14_102318_create_posts_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
+            $table->foreignId('author_id')->constrained(
+                table: 'users', indexName: 'posts_author_id'
+            );
+            // $table->unsignedBigInteger('author_id');
+            // $table->foreign('author_id')->references('id')->on('users');
             $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();
