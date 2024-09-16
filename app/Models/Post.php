4589dd54b2741e9026b2a2a9818117a249cecdd4
guitiorr <officialglude12345@gmail.com>
2024-09-16 13:50:59 +0700

    added BelongsTo type in Post model relation to User model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'author_id');
     }
