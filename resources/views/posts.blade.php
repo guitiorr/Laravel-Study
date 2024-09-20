48c7d2d8454eef5d7e2e96a3acab40a64e294fc3
guitiorr <officialglude12345@gmail.com>
2024-09-20 12:41:01 +0700

    readjust posts view content

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -64,28 +64,31 @@
                         <article
                             class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                             <div class="flex justify-between items-center mb-5 text-gray-500">
-                                <span
-                                    class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                    {{ $post->category->name }}
-                                </span>
-                                <span class="text-sm">14 days ago</span>
+                                <a href="/categories/{{ $post->category->slug }}" class="">
+                                    <span
+                                        class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                        {{ $post->category->name }}
+                                    </span>
+                                </a>
+                                <span class="text-sm">{{ $post->created_at->diffForHumans() }}</span>
                             </div>
                             <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
                                     href="#">How to quickly deploy a static website</a></h2>
-                            <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to
-                                bootstrap lots of websites and are becoming the basis for a variety of tools that even
-                                influence both web designers and developers influence both web designers and developers.
+                            <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+                                {{ Str::limit($post->body, 150) }}
                             </p>
                             <div class="flex justify-between items-center">
                                 <div class="flex items-center space-x-4">
                                     <img class="w-7 h-7 rounded-full"
                                         src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
                                         alt="Jese Leos avatar" />
-                                    <span class="font-medium dark:text-white">
-                                        Jese Leos
-                                    </span>
+                                    <a href="/authors/{{ $post->user->username }}" class="">
+                                        <span class="font-medium dark:text-white">
+                                            {{ $post->user->name }}
+                                        </span>
+                                    </a>
                                 </div>
-                                <a href="#"
+                                <a href="/posts/{{ $post->slug }}"
                                     class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
                                     Read more
                                     <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
